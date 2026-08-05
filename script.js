ba42727b32621ae0ccd1ef1ae7121fd0610251e8
theroyalcut41-pixel <theroyalcut41@gmail.com>
2026-08-05 18:54:44 +0500

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,16 @@ document.querySelectorAll('a[href^="#"]').forEach(link => {
 });
 
 console.log("The Royal Cut Website Loaded Successfully!");
+// The Royal Cut by Ali Raza
+
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("The Royal Cut website loaded successfully!");
+
+  const btn = document.querySelector(".btn");
+
+  if (btn) {
+    btn.addEventListener("click", function () {
+      alert("Thank you for choosing The Royal Cut by Ali Raza!\nYou will now be redirected to WhatsApp.");
+    });
+  }
+});
